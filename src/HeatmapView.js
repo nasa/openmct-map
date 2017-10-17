770a330df986c70cb67a8a4933d4246df524c958
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-10-17 13:33:58 -0700

    Wire components into HeatmapView

--- src/HeatmapView.js
+++ src/HeatmapView.js
@@ -1,13 +1,37 @@
-define([], function () {
+define([
+    './HeatmapColors',
+    './HeatmapController',
+    './HeatmapModel',
+    './HeatmapRenderer',
+    'zepto'
+], function (
+    HeatmapColors,
+    HeatmapController,
+    HeatmapModel,
+    HeatmapRenderer,
+    $
+) {
     function HeatmapView(domainObject, openmct) {
+        this.domainObject = domainObject;
+        this.openmct = openmct;
     }
 
-    HeatmapView.prototype.show = function () {
+    HeatmapView.prototype.show = function (container) {
+        var $canvas = $("<canvas>");
+        var renderer = new HeatmapRenderer($canvas[0], new HeatmapColors());
+        var model = new HeatmapModel();
+        var controller = new HeatmapController(model, renderer, this.openmct);
 
+        this.unobserve = controller.observe(this.domainObject);
+
+        $(container).append($canvas);
     };
 
     HeatmapView.prototype.destroy = function () {
-
+        if (this.unobserve) {
+            this.unobserve();
+            delete this.unobserve;
+        }
     };
 
     return HeatmapView;
