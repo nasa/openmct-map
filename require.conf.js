d010ba113f4c5339c5e7ab8deb6fcca615295c9a
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-10-17 10:33:56 -0700

    Configure require to find openmct

--- require.conf.js
+++ require.conf.js
@@ -1,5 +1,6 @@
 requirejs.config({
     paths: {
-        heatmap: "node_modules/heatmap.js/build/heatmap.min"
+        heatmap: "node_modules/heatmap.js/build/heatmap.min",
+        openmct: "node_modules/openmct/dist/openmct.js"
     }
 });
