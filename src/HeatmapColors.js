c05198e90fed898163fcce7b3074f519e5cb507d
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-10-19 10:37:28 -0700

    Generate heatmap-style colors

--- src/HeatmapColors.js
+++ src/HeatmapColors.js
@@ -9,8 +9,16 @@ define([], function () {
             return "rgb(127,127,127)";
         }
 
-        var v = Math.floor(((value - this.minimum) / this.maximum) * 255);
-        return "rgb(" + [v, 255, 255].join(',') + ")";
+        var v = (value - this.minimum) / this.maximum;
+        var r = (Math.max(v - 0.5, 0) * 2);
+        var g = 1 - 2 * Math.abs(v - 0.5);
+        var b = Math.max(0.5 - v, 0) * 2;
+
+        console.log(r, g, b, v);
+
+        return "rgb(" + [r, g, b].map(function (c) {
+            return Math.min(Math.max(Math.floor(c * 255), 0), 255);
+        }).join(',') + ")";
     };
 
     return HeatmapColors;
