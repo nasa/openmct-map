1fb52f35bed98bf8df0bcc7635d6b840cc42373e
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-10-23 12:11:01 -0700

    Add new dependencies to require config

--- require.conf.js
+++ require.conf.js
@@ -1,5 +1,7 @@
 requirejs.config({
     paths: {
-        openmct: "node_modules/openmct/dist/openmct"
+        openmct: "node_modules/openmct/dist/openmct",
+        text: "node_modules/requirejs-text/text",
+        vue: "node_modules/vue/dist/vue.min"
     }
 });
