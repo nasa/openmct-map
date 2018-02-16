df1ac9a5fdc1f8ae1559caae2dd5726844089026
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-15 16:39:39 -0800

    Add type for the traverse map

--- src/openmct-map.js
+++ src/openmct-map.js
@@ -1,5 +1,56 @@
 export default function mapPlugin(options) {
     return function (openmct) {
-
+        openmct.types.addType('view.traverse', {
+            name: 'Traverse Map',
+            description: 'A visualization of a rover traverse.',
+            key: 'view.map',
+            cssClass: 'icon-object',
+            creatable: true,
+            initialize: function (obj) {
+            },
+            form: [
+                {
+                    key: "left",
+                    name: "Left X",
+                    control: "textfield",
+                    required: true
+                },
+                {
+                    key: "top",
+                    name: "Top Y",
+                    control: "textfield",
+                    required: true
+                },
+                {
+                    key: "right",
+                    name: "Right X",
+                    control: "textfield",
+                    required: true
+                },
+                {
+                    key: "bottom",
+                    name: "Bottom Y",
+                    control: "textfield",
+                    required: true
+                },                {
+                    key: "left",
+                    name: "Left X",
+                    control: "textfield",
+                    required: true
+                },
+                {
+                    key: "x",
+                    name: "X Telemetry",
+                    control: "textfield",
+                    required: true
+                },
+                {
+                    key: "y",
+                    name: "y Telemetry",
+                    control: "textfield",
+                    required: true
+                }
+            ]
+        });
     };
 };
