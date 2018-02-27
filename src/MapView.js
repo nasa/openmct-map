a7e546e0fb8be1883503bbedd5c6f4fff9db461b
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-26 16:14:43 -0800

    OpenLayers hello world

--- src/MapView.js
+++ src/MapView.js
@@ -6,10 +6,25 @@ export class MapView {
     }
     
     show(element) {
-        
+        return new Map({
+            target: element,
+            layers: [
+                new TileLayer({
+                    source: new XYZ({
+                        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    })
+                })
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2
+            })
+        });
     }
     
     destroy() {
-        
+        if (this.map) {
+
+        }
     }
 }
