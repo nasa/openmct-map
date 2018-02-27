9be8845f08fc1f887133db9ad299aae769119d43
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-26 16:10:10 -0800

    Add skeleton MapView methods

---
+++ src/MapView.js
@@ -0,0 +1,15 @@
+import 'ol/ol.css';
+
+export class MapView {
+    constructor(domainObject) {
+        
+    }
+    
+    show(element) {
+        
+    }
+    
+    destroy() {
+        
+    }
+}
