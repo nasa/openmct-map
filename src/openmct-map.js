ddf8f0d868065db0c6a384219e38205fedaf7aa2
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-28 10:46:55 -0800

    Simplify map type

--- src/openmct-map.js
+++ src/openmct-map.js
@@ -27,50 +27,7 @@ export default function mapPlugin(options) {
             creatable: true,
             initialize: function (obj) {
                 obj.layers = [];
-            },
-            form: [
-                {
-                    key: "left",
-                    name: "Left X",
-                    control: "textfield",
-                    required: true
-                },
-                {
-                    key: "top",
-                    name: "Top Y",
-                    control: "textfield",
-                    required: true
-                },
-                {
-                    key: "right",
-                    name: "Right X",
-                    control: "textfield",
-                    required: true
-                },
-                {
-                    key: "bottom",
-                    name: "Bottom Y",
-                    control: "textfield",
-                    required: true
-                },                {
-                    key: "left",
-                    name: "Left X",
-                    control: "textfield",
-                    required: true
-                },
-                {
-                    key: "x",
-                    name: "X Telemetry",
-                    control: "textfield",
-                    required: true
-                },
-                {
-                    key: "y",
-                    name: "y Telemetry",
-                    control: "textfield",
-                    required: true
-                }
-            ]
+            }
         });
 
         (openmct.mainViews || openmct.objectViews).addProvider({
