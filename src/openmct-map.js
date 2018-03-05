9511ef94f3a0f28f9dbb741968436938fbef8260
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-03-05 15:08:03 -0800

    Add JSON entry control

--- src/openmct-map.js
+++ src/openmct-map.js
@@ -3,6 +3,32 @@ import OpenLayersMapView from './OpenLayersMapView';
 
 export default function mapPlugin(options) {
     return function (openmct) {
+        function JSONController($scope) {
+            $scope.jsonText = JSON.stringify($scope.ngModel[$scope.field]);
+            $scope.$watch("jsonText", function (value) {
+                try {
+                    $scope.ngModel[$scope.field] =
+                        JSON.parse(value);
+                    $scope.ngModelController.$setValidity('json', true);
+                } catch (e) {
+                    $scope.ngModelController.$setValidity('json', false);
+                }
+            });
+        }
+
+        openmct.legacyExtension("controllers", {
+            key: "JSONController",
+            implementation: function ($scope) {
+                return new JSONController($scope);
+            },
+            depends: ["$scope"]
+        });
+
+        openmct.legacyExtension("controls", {
+            key: "json",
+            template: "<code ng-controller='JSONController'><textarea ng-model='jsonText'></textarea></code>"
+        });
+
         openmct.types.addType('view.traverse', {
             name: 'Traverse Map',
             description: 'A visualization of a rover traverse.',
@@ -16,7 +42,7 @@ export default function mapPlugin(options) {
                 {
                     key: "layers",
                     name: "Layers",
-                    control: "textarea",
+                    control: "json",
                     required: true,
                     cssClass: "l-textarea-sm"
                 }
