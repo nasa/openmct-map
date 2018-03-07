b5931171d654320b1d136fe0a106969fffc53419
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-03-06 17:05:02 -0800

    Convert to factory pattern to allow complicated layers

--- src/TraverseView.js
+++ src/TraverseView.js
@@ -1,14 +1,19 @@
 export default class TraverseView {
-    constructor(domainObject, map) {
+    constructor(domainObject, map, layerFactory) {
         this.domainObject = domainObject;
         this.map = map;
+        this.layerFactory = layerFactory;
     }
 
     show(element) {
         var div = document.createElement('div');
         element.appendChild(div);
         this.map.clear();
-        this.domainObject.layers.forEach(this.map.layer.bind(this.map));
+        this.domainObject.layers
+            .map(this.layerFactory.create.bind(this.layerFactory))
+            .forEach(function (layer) {
+                layer.show(this.map);
+            }, this);
         this.map.show(div);
     }
 
