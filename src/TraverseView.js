aedaae6aa581b24f9d28fe061a9cb1499742432b
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-03-06 16:58:03 -0800

    Dispatch layer creation to handler

--- src/TraverseView.js
+++ src/TraverseView.js
@@ -1,16 +1,52 @@
+class LayerHandler {
+    constructor(map) {
+        this.map = map;
+    }
+
+    handle(layer) {
+        this[layer.type](layer);
+    }
+
+    image(options) {
+        this.map.image(
+            options.url,
+            options.left,
+            options.bottom,
+            options.right,
+            options.top
+        );
+    }
+
+    plan(options) {
+
+    }
+
+    path(options) {
+
+    }
+
+    heatmap(options) {
+
+    }
+};
+
 export default class TraverseView {
     constructor(domainObject, map) {
         this.domainObject = domainObject;
         this.map = map;
+        this.handler = new LayerHandler(map);
+    }
 
-        this.map.image("data/ldem.png", -47, 88, -48, 87);
-        this.map.traverse("data/plan.kml");
+    buildLayers() {
+        this.map.clear();
+        this.domainObject.layers.forEach(this.handler.handle.bind(this.handler));
     }
 
     show(element) {
         var div = document.createElement('div');
         element.appendChild(div);
         this.map.show(div);
+        buildLayers();
     }
 
     destroy() {
