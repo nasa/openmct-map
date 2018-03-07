3e99d18a516b3f4a158e2e2e7f3028f0eee6edf6
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-03-07 13:39:58 -0800

    Flesh out telemetry combination

--- src/Telemetry.js
+++ src/Telemetry.js
@@ -6,21 +6,22 @@ export default class Telemetry extends EventEmitter {
         this.ids = ids;
         this.metadata = {};
         this.unsubscribes = {};
+        this.latest = {};
     }
-    
+
     activate() {
         let properties = Object.keys(this.ids);
-        let bounds = this.openmct.time.bounds();  
+        let bounds = this.openmct.time.bounds();
         let data = {};
         this.emit('reset');
-        this.loading = true;        
+        this.loading = true;
         Promise.all(properties.map(function (property) {
             let idParts = domainObject[property].split(":");
             let identifier = idParts.length > 1 ?
-                { namespace: idParts[0], key: idParts[1] } : idParts[0];   
-            let add = this.add.bind(this, property);             
-            return this.openmct.objects.get(identifer).then(function (object) {                
-                this.metadata[property] = 
+                { namespace: idParts[0], key: idParts[1] } : idParts[0];
+            let add = this.add.bind(this, property);
+            return this.openmct.objects.get(identifer).then(function (object) {
+                this.metadata[property] =
                     this.openmct.telemetry.getMetadata(obj);
                 this.unsubscribes[property] =
                     this.openmct.telemetry.subscribe(object, add);
@@ -34,27 +35,59 @@ export default class Telemetry extends EventEmitter {
                 })
             }))
         }.bind(this)).then(function () {
+            let indexes =
+                properties.reduce((indexes, property) => indexes[property] = 0, {});
+            let domain = this.openmct.time.timeSystem().key;
+            let keys = properties.reduce(function (keys, property) {
+                var meta = this.metadata[property].valuesForHints(['domain']).find(function (m) {
+                    return m.source === domain || m.key === domain;
+                });
+                keys[property] = meta.source || meta.key || domain;
+                return keys;
+            }.bind(this));
+            let remaining = (property) => indexes[property] < data[property].length;
+            let time = (property) => data[property][indexes[property]][keys[property]];
+
             this.loading = false;
-            // Add all the historical data
-            
-            // Add all pending realtime data
+
+            while (properties.some(remaining)) {
+                let timestamp =
+                    properties.filter(remaining).map(time).reduce((a, b) => a < b ? a : b);
+
+                properties.forEach(function (property) {
+                    while (remaining(property) && time(property) <= timestamp) {
+                        this.add(property, data[property][indexes[property]]);
+                        indexes[property] += 1;
+                    }
+                });
+            }
+
             this.queue.forEach(function (pending) {
                 this.add(pending.property, pending.datum);
             }, this);
         }.bind(this));
     }
-    
+
     add(property, datum) {
         if (this.loading) {
             this.queue.push({ property, datum });
         } else {
             // Add datum, emit event if appropriate
+            let metadata = this.metadata[property];
+            let metadataValues = metadata.valuesForHints(["range"]);
+            if (metadataValues.length > 0) {
+                this.latest[property] = datum[metadataValues[0].key];
+                if (Object.keys(this.latest).length === Object.keys(this.ids).length) {
+                    this.emit('add', this.latest);
+                    this.latest = {};
+                }
+            }
         }
     }
-    
+
     destroy() {
         Object.keys(this.unsubscribes).forEach(function (property) {
             unsubscribes[property]();
         });
-    }    
+    }
 }
